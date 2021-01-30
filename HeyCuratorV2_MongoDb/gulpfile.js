98e63e6a94dd01b72c33c8d8c60dca6212692d0c
Timsy Magnus <tmherron09@gmail.com>
2021-01-30 15:10:34 -0600

    Fixed css purge in gulp
    defaultextractor for purgecss in gulp is now functionion correctly with regex solution on Tailwind CSS docs.
    Cleaned up variable css calls in views so that full names appear, allowing

--- HeyCuratorV2_MongoDb/gulpfile.js
+++ HeyCuratorV2_MongoDb/gulpfile.js
@@ -1,15 +1,35 @@
 ﻿const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const purgecss = require('gulp-purgecss');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task('css', () => {
-    const postcss = require('gulp-postcss');
-    const purgecss = require('gulp-purgecss');
+gulp.task('css:prod', () => {
+    
 
     return gulp.src('./Styles/site.css')
+        .pipe(sourcemaps.init())
         .pipe(postcss([
             require('precss'),
             require('tailwindcss'),
             require('autoprefixer')
         ]))
-        //.pipe(purgecss({ content: ['**/*.html', '**/*.razor', '**/*.cshtml', 'Views/**/*.cshtml', 'Views/Shared/_*.cshtml' ] }))
+        .pipe(purgecss({
+            content: ['**/*.html', '**/*.razor', '**/*.cshtml', 'Views/**/*.cshtml', 'Views/Shared/_*.cshtml'],
+            defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+        }))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('css:dev', () => {
+    return gulp.src('./Styles/site.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
         .pipe(gulp.dest('./wwwroot/css/'));
 });
