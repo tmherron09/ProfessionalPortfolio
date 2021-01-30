f1489043ded7e18193f4a5aefca377260b95dc4a
Timsy Magnus <tmherron09@gmail.com>
2021-01-30 03:12:12 -0600

    Added Tailwind pipeline
    PurgeCSS is still removing some width styling, temp removed until fixed. CSS is huge but it is still only dev.
    Added a larger variety of colors to tailwind config.

---
+++ HeyCuratorV2_MongoDb/gulpfile.js
@@ -0,0 +1,15 @@
+﻿const gulp = require('gulp');
+
+gulp.task('css', () => {
+    const postcss = require('gulp-postcss');
+    const purgecss = require('gulp-purgecss');
+
+    return gulp.src('./Styles/site.css')
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        //.pipe(purgecss({ content: ['**/*.html', '**/*.razor', '**/*.cshtml', 'Views/**/*.cshtml', 'Views/Shared/_*.cshtml' ] }))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
